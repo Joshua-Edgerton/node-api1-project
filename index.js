d67ee85ec45787e6ca5bf5e94bd316d45ce89ad8
Joshua Edgerton <joshuaxedgerton@gmail.com>
2019-12-02 15:10:59 -0600

    First GET working and showing data

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// implement your API here
+const express = require('express'); //CommonJS modules
+const db = require('./data/db'); // import database file
+const server = express();
+server.use(express.json()); //needed to parse json from body
+
+
+server.get('/', (req, res) => {
+    res.send({ api: 'up and running...'})
+ })
+
+ server.post('/api/users', (req, res) => {
+    //get data the client sent
+    const userData = req.body; //express does not know how to parse json
+
+    //call the db and add the hub
+    db.add(userData)
+    .then(user => {
+        res.status(201).json(user);
+    })
+    .catch(error => {
+        console.log('error on POST /api/users', error);
+        res.status(500).json({ errorMessage: 'error posting a new user', error});
+    });
+});
+
+ server.get('/api/users', (req, res) => {
+    db.find()
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(error => {
+        console.log('error on GET /api/users', error);
+    });
+ });
+
+ server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    db.find(id)
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(error => {
+        console.log('error on GET /api/users/:id', error);
+    });
+ });
+
+const port = 4500;
+server.listen(port, () => 
+console.log(`\n ** API running on port ${port} \n`)
+);
